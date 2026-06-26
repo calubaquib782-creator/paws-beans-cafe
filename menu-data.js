075fe412d6a60e5435b95e3e90a1f2499aab5c0b
calubaquib782-creator <calubaquib782@gmail.com>
2026-06-26 19:40:38 +0400

    Initial Paws & Beans Cafe Website

---
+++ menu-data.js
@@ -0,0 +1,121 @@
+const MENU_ITEMS = [
+  {
+    "name": "Espresso Single",
+    "category": "Hot Coffee",
+    "price": "12",
+    "image": "images/espresso-single.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Espresso Double",
+    "category": "Hot Coffee",
+    "price": "15",
+    "image": "images/espresso-double.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Macchiato Single",
+    "category": "Hot Coffee",
+    "price": "13",
+    "image": "images/macchiato-single.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Macchiato Double",
+    "category": "Hot Coffee",
+    "price": "16",
+    "image": "images/macchiato-double.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Piccolo",
+    "category": "Hot Coffee",
+    "price": "15",
+    "image": "images/piccolo.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Flat White",
+    "category": "Hot Coffee",
+    "price": "18",
+    "image": "images/flat-white.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Café Latte",
+    "category": "Hot Coffee",
+    "price": "18",
+    "image": "images/caf-latte.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Spanish Latte",
+    "category": "Hot Coffee",
+    "price": "20",
+    "image": "images/spanish-latte.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Black Coffee",
+    "category": "Hot Coffee",
+    "price": "14",
+    "image": "images/black-coffee.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Iced Latte",
+    "category": "Iced Drinks",
+    "price": "20",
+    "image": "images/iced-latte.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Iced Mocha",
+    "category": "Iced Drinks",
+    "price": "22",
+    "image": "images/iced-mocha.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Iced Spanish Latte",
+    "category": "Iced Drinks",
+    "price": "22",
+    "image": "images/iced-spanish-latte.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Iced Chocolate",
+    "category": "Iced Drinks",
+    "price": "20",
+    "image": "images/iced-chocolate.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Iced Black Coffee",
+    "category": "Iced Drinks",
+    "price": "18",
+    "image": "images/iced-black-coffee.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Mango Iced Tea",
+    "category": "Iced Drinks",
+    "price": "18",
+    "image": "images/mango-iced-tea.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Matcha on Ice",
+    "category": "Matcha",
+    "price": "22",
+    "image": "images/matcha-on-ice.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  },
+  {
+    "name": "Zen Matcha Warmth",
+    "category": "Matcha",
+    "price": "20",
+    "image": "images/zen-matcha-warmth.jpg",
+    "description": "Freshly prepared at Paws & Beans Cafe."
+  }
+];
