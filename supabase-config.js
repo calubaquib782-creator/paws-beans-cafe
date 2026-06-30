5e196c09753ccdf722a9fa51cc77817faacff7b9
calubaquib782-creator <calubaquib782@gmail.com>
2026-06-30 20:37:22 +0400

    Connect website to Supabase

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// Paws & Beans Cafe Supabase Config
+// Paste your Supabase Publishable/Anon key below.
+const SUPABASE_URL = 'https://hygplvjawvbrjakqlrgo.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_SuvS2vU1VFrW8CT89QvFoQ_Zh8ZiLBO';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
